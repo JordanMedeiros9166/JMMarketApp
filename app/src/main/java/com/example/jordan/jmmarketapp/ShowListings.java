566fae0b24b18431a986f03b9e61a02e4270dc21
JordanMedeiros9166 <jordanmedeiros13@hotmail.com>
2017-12-29 03:16:03 -0500

    JM Market App

--- app/src/main/java/com/example/jordan/jmmarketapp/ShowListings.java
+++ app/src/main/java/com/example/jordan/jmmarketapp/ShowListings.java
@@ -1,45 +1,37 @@
 package com.example.jordan.jmmarketapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ShowListings extends AppCompatActivity {
     int count =0;
+    ListView v;
     private AppDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         db = AppDatabase.getAppDatabase(getApplicationContext());
         setContentView(R.layout.activity_show_listings);
-
+         v = (ListView) findViewById(R.id.listView1);
         fetchInfo();
     }
 
 
+
     public void fetchInfo(){
-
-        StringBuilder sb = new StringBuilder();
-        //List<House> h = db.houseDao().getHouse(count);
         List<House> h = db.houseDao().getAllHouse();
-
-        ListView v = (ListView) findViewById(R.id.listings);
-
+        ArrayList<String> ar = new ArrayList<>();
         for(House house : h){
-            sb.append(String.format("Name: %s   Price: %s   Type: %s - \n Location: %s , Id: %s \n",
-                    house.getUsername(),house.getPrice(),house.getType(),house.getLocation(),house.getUserId()));
+            ar.add(""+ (String.format("Name: %s  \nPrice: %s\nType: %s  \nLocation: %s , Id<testing>: %s \n",
+                    house.getUsername(),house.getPrice(),house.getType(),house.getLocation(),house.getUserId())));
         }
-
-        List<String> sbItems = new ArrayList<>(Arrays.asList(sb.toString()));
-        ArrayAdapter<String> items = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, sbItems);
+        ArrayAdapter<String> items = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, ar);
         v.setTextFilterEnabled(true);
         v.setAdapter(items);
-
-
     }
 }
